ca5e204f85f0918cc47c4243787d8229d4e8559e
Hoang Minh Quan <92797807+hminhquan2002@users.noreply.github.com>
2022-12-20 21:50:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
     titleWeb: "BẢN KIỂM ĐIỂM",
     introTitle: 'Narciso trắng ❤️',
-    introDesc: `Em ơi! Anh đã sai rồi
-    Xin em tha thứ lỗi lầm của anh.
+    introDesc: `Em ơi! Anh đã sai rồi,
+    xin em tha thứ lỗi lầm của anh.
     Ngàn lời xin lỗi chân thành,
-    Mong em tha thứ cho anh vơi buồn. `,
+    mong em tha thứ cho anh vơi buồn. `,
     btnIntro: 'Vâng 🥺',
-    title: 'Anh yêu em nhiều hơn cả',
+    title: 'Anh yêu em nhiều hơn cả 💙 ',
     desc: '',
-    btnYes: 'Thế giới này',
+    btnYes: 'Thế giới này 🌏',
     btnNo: 'Anh không yêu em',
     question: 'Em sẽ bỏ qua những lời nói vô nghĩa đó của anh và không được buồn, em phải luôn luôn vui vẻ nhé',
     btnReply: 'Em yêu anh <3',
